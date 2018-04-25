fd3c34d4318fb8fcfe84df2d2f3af9506a697f06
reptilebeast <reptilebeats@gmail.com>
2018-04-25 11:20:03 +0100

    Fmod example
    Added a simple exaple script to play a 3d sound on key press 1 to the main scene

---
+++ Assets/Scripts/Fmod_staticSound_example.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fmod_staticSound_example : MonoBehaviour {
+
+    //this is required to get a reference to the event from fmod
+    [FMODUnity.EventRef]
+    string switchE = "event:/Morse Key/Switch"; //event path can be found the fmod browser
+
+    //we store an instance of the event so that we can call it
+    FMOD.Studio.EventInstance switchEInst;
+
+    void Start()
+    {
+        //we need to set the event to the instance and set the location of the instance (will only work on static objects)
+        switchEInst = FMODUnity.RuntimeManager.CreateInstance(switchE);
+        switchEInst.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(this.transform, GetComponent<Rigidbody>()));
+    }
+
+    void Update ()
+    {
+        if (Input.GetKeyUp("1"))
+        {
+            //play the sound
+           switchEInst.start();
+        }
+	}
+}
