625c7c24a96717bfde11267906e5c2d9a5f1893f
Alcwyn Parker <alcwynparker@hotmail.co.uk>
2018-08-22 14:50:14 +0100

    combined the positioning and scenemanager

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,21 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour {
 
-    private string[] availableScenes = { "BinaryTree-morse-key", "Galvanometer-models" };
+    public PositionList sceneList;
     public int startingScene = 0;
+    public bool scenesLoaded = false;
 
-	// Use this for initialization
-	void Start () {
-        //SceneManager.LoadScene(availableScenes[startingScene], LoadSceneMode.Additive);
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(GetPositions());
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    IEnumerator GetPositions()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://192.168.0.102:5000/sqlite/position/all"))
+        {
+
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Positions Loaded!");
+            }
+
+            this.sceneList = JsonUtility.FromJson<PositionList>(www.downloadHandler.text);
+            this.scenesLoaded = true;
+            Debug.Log("Number of Positions: " + sceneList.positions.Length);
+
+        }
+    }
 }
